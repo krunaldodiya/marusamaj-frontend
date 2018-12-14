8b12c554e3d6ab23a2b3b9340921dff641fdb9e5
krunaldodiya <kunal.dodiya1@gmail.com>
2018-12-14 21:26:18 +0530

    done

--- src/components/ManageCaste/form_caste.js
+++ src/components/ManageCaste/form_caste.js
@@ -1,30 +1,26 @@
+import { Content, List, ListItem, Text, Container } from "native-base";
 import React from "react";
-import { FlatList, Text, View } from "react-native";
-import { Container } from "native-base";
-
-renderItem = (data, navigation) => {
-  const { item } = data;
-
-  console.log(item);
-  
-  return (
-    <View style={{ margin: 5 }}>
-      <Text>{item.name}</Text>
-    </View>
-  );
-};
 
 const CasteForm = props => {
-  const { navigation, castes } = props;
+  const { castes } = props;
   const { data } = castes;
 
   return (
-    <Container style={{ padding: 5 }}>
-      <FlatList
-        data={data}
-        keyExtractor={(_, index) => index.toString()}
-        renderItem={data => renderItem(data, navigation)}
-      />
+    <Container>
+      <Content>
+        <List
+          dataArray={data}
+          renderRow={item => {
+            return (
+              <ListItem>
+                <Text>
+                  hello
+                </Text>
+              </ListItem>
+            );
+          }}
+        />
+      </Content>
     </Container>
   );
 };
