6cb0e3719c17fe9c37a0e893ab76266f991cb088
krunaldodiya <kunal.dodiya1@gmail.com>
2018-12-23 22:08:55 +0530

    done

--- src/components/AddRelation/content_body.js
+++ src/components/AddRelation/content_body.js
@@ -1,4 +1,4 @@
-import { Text, View } from "native-base";
+import { Text, View, Button } from "native-base";
 import React from "react";
 import theme from "../../libs/theme";
 import { TouchableOpacity } from "react-native";
@@ -32,13 +32,35 @@ class ContentBody extends React.Component {
     this.state = {
       user_relation: null,
       relative_relation: null,
-      request_status: null
+      relation: null
     };
   }
 
+  componentDidMount() {
+    this.setRelation(this.props);
+  }
+
+  componentWillReceiveProps(props) {
+    this.setRelation(props);
+  }
+
+  setRelation = props => {
+    const { auth, navigation } = props;
+    const { authUser } = auth;
+    const { relative } = navigation.state.params;
+
+    if (authUser.relatives.length) {
+      const relation = authUser.relatives.filter(
+        rel => relative.id === rel.relative_id
+      )[0];
+
+      this.setState({ relation });
+    }
+  };
+
   render() {
     const { navigation, auth, addRelation } = this.props;
-    const { user_relation, relative_relation } = this.state;
+    const { user_relation, relative_relation, relation } = this.state;
     const { authUser } = auth;
     const { relative } = navigation.state.params;
 
@@ -58,69 +80,114 @@ class ContentBody extends React.Component {
           </Text>
         </View>
 
-        <View style={{ flex: 1 }}>
-          {relationship.map((user_relation_data, index) => {
-            const relative_relation_data =
-              relative_relations[user_relation_data][authUser.gender];
+        {!relation && (
+          <View style={{ flex: 1 }}>
+            <View style={{ flex: 1 }}>
+              {relationship.map((user_relation_data, index) => {
+                const relative_relation_data =
+                  relative_relations[user_relation_data][authUser.gender];
 
-            return (
-              <View
-                style={{ marginHorizontal: 20, marginVertical: 10 }}
-                key={index}
-              >
-                <Text
-                  onPress={() =>
-                    this.setState({
-                      user_relation: user_relation_data,
-                      relative_relation: relative_relation_data
-                    })
-                  }
-                  style={{
-                    fontSize: 18,
-                    fontFamily:
-                      user_relation === user_relation_data
-                        ? theme.fonts.TitilliumWebSemiBold
-                        : theme.fonts.TitilliumWebRegular,
-                    color:
-                      user_relation === user_relation_data ? "green" : "black"
-                  }}
-                >
-                  {user_relation_data}
-                </Text>
-              </View>
-            );
-          })}
-        </View>
+                return (
+                  <View
+                    style={{ marginHorizontal: 20, marginVertical: 10 }}
+                    key={index}
+                  >
+                    <Text
+                      onPress={() =>
+                        this.setState({
+                          user_relation: user_relation_data,
+                          relative_relation: relative_relation_data
+                        })
+                      }
+                      style={{
+                        fontSize: 18,
+                        fontFamily:
+                          user_relation === user_relation_data
+                            ? theme.fonts.TitilliumWebSemiBold
+                            : theme.fonts.TitilliumWebRegular,
+                        color:
+                          user_relation === user_relation_data
+                            ? "green"
+                            : "black"
+                      }}
+                    >
+                      {user_relation_data}
+                    </Text>
+                  </View>
+                );
+              })}
+            </View>
 
-        {user_relation && (
-          <TouchableOpacity
-            disabled={!user_relation}
-            onPress={() =>
-              addRelation({
-                relative_id: relative.id,
-                user_relation,
-                relative_relation
-              })
-            }
-            style={{
-              alignSelf: "center",
-              backgroundColor: "#d80402",
-              padding: 7,
-              borderRadius: 30,
-              width: 140
-            }}
-          >
-            <Text
+            <TouchableOpacity
+              disabled={!user_relation}
+              onPress={() =>
+                addRelation({
+                  relative_id: relative.id,
+                  user_relation,
+                  relative_relation
+                })
+              }
               style={{
-                fontSize: 16,
-                fontFamily: theme.fonts.TitilliumWebSemiBold,
-                color: "white",
-                textAlign: "center"
+                alignSelf: "center",
+                backgroundColor: "#d80402",
+                padding: 7,
+                borderRadius: 30,
+                width: 140
               }}
             >
-              Send Request
-            </Text>
-          </TouchableOpacity>
+              <Text
+                style={{
+                  fontSize: 16,
+                  fontFamily: theme.fonts.TitilliumWebSemiBold,
+                  color: "white",
+                  textAlign: "center"
+                }}
+              >
+                Send Request
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )}
+
+        {relation && (
+          <View style={{ flex: 1 }}>
+            <View style={{ flex: 1 }} />
+
+            <View
+              style={{
+                justifyContent: "space-around",
+                flexDirection: "row"
+              }}
+            >
+              <View style={{ margin: 10 }}>
+                <Button
+                  small
+                  rounded
+                  bordered
+                  success={relation.status ? true : false}
+                  danger={relation.status ? false : true}
+                >
+                  <Text>
+                    {relation.status
+                      ? `Added as ${relation.user_relation}`
+                      : `Request as ${relation.user_relation}`}
+                  </Text>
+                </Button>
+              </View>
+
+              <View style={{ margin: 10 }}>
+                <Button
+                  small
+                  rounded
+                  bordered
+                  success={relation.status ? true : false}
+                  danger={relation.status ? false : true}
+                >
+                  <Text>cancel</Text>
+                </Button>
+              </View>
+            </View>
+          </View>
         )}
       </View>
     );
